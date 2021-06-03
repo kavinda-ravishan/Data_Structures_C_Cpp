392129acc42c7471b793b5aabc5b1ea3ed0b4317
kavinda-ravishan <kvnravishan@outlook.com>
2021-06-03 12:35:19 +0530

    binary tree node.

--- Data_Structures_C_Cpp/Application.cpp
+++ Data_Structures_C_Cpp/Application.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
+/*
 #include "SinglyLinkedList.h"
 #include "DoublyLinkedList.h"
 #include "LinkedList.h"
+*/
 
+template <typename T>
+struct BinTreeNode
+{
+	T m_data;
+	BinTreeNode<T>* m_leftPtr;
+	BinTreeNode<T>* m_rightPtr;
+
+	BinTreeNode(T data);
+};
 
 
 int main(int argc, char** args) {
 	
-	SinglyLinkedListTest::TestAll();
-	DoublyLinkedListTest::TestAll();
-	StackQueuTest::TestAll();
-	
+	BinTreeNode<int>* node = new BinTreeNode<int>(10);
+
+	std::cout << "Data : " << node->m_data << std::endl;
+	std::cout << "Left : " << node->m_leftPtr << std::endl;
+	std::cout << "Right : " << node->m_rightPtr << std::endl;
+
 	return 0;
 }
 
+// --- Node --- //
+
+template<typename T>
+BinTreeNode<T>::BinTreeNode(T data) :m_leftPtr(nullptr), m_rightPtr(nullptr), m_data(data) {}
