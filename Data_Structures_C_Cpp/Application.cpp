a885a023b052333bebe5e5a3b6818b29bb0ec206
kavinda-ravishan <kvnravishan@outlook.com>
2021-06-07 12:04:36 +0530

    Search recursive.

--- Data_Structures_C_Cpp/Application.cpp
+++ Data_Structures_C_Cpp/Application.cpp
@@ -21,11 +21,13 @@ private:
 	BinaryTreeNode<T>* m_rootPrt;
 	BinaryTreeNode<T>* GetANewNode(T data) const;
 	BinaryTreeNode<T>* Insert(T data, BinaryTreeNode<T>* rootPtr);
+	bool Search(T data, BinaryTreeNode<T>* rootPtr) const;
 public:
 	BinarySearchTree();
 
 	void Insert(T data);
 	void InsertRec(T data);
+	bool SearchRec(T data) const;
 };
 
 int main(int argc, char** args) {
@@ -35,6 +37,18 @@ int main(int argc, char** args) {
 	tree.InsertRec(10);
 	tree.InsertRec(20);
 	tree.InsertRec(25);
+	tree.Insert(16);
+	tree.Insert(11);
+	tree.Insert(21);
+	tree.Insert(26);
+
+	std::cout << "Search for Number {" << 15 << "} : " << tree.SearchRec(15) << std::endl;
+	std::cout << "Search for Number {" << 26 << "} : " << tree.SearchRec(26) << std::endl;
+	std::cout << "Search for Number {" << 16 << "} : " << tree.SearchRec(16) << std::endl;
+
+	std::cout << "Search for Number {" << 17 << "} : " << tree.SearchRec(17) << std::endl;
+	std::cout << "Search for Number {" << 27 << "} : " << tree.SearchRec(27) << std::endl;
+	std::cout << "Search for Number {" << 18 << "} : " << tree.SearchRec(18) << std::endl;
 
 	return 0;
 }
@@ -50,6 +64,30 @@ template<typename T>
 BinaryTreeNode<T>* BinarySearchTree<T>::GetANewNode(T data) const
 {
 	return new BinaryTreeNode<T>(data);
+}
+
+template<typename T>
+BinaryTreeNode<T>* BinarySearchTree<T>::Insert(T data, BinaryTreeNode<T>* rootPtr)
+{
+	if (rootPtr == nullptr) {
+		rootPtr = GetANewNode(data);
+	}
+	else if (data <= rootPtr->m_data) {
+		rootPtr->m_leftPtr = Insert(data, rootPtr->m_leftPtr);
+	}
+	else {
+		rootPtr->m_rightPtr = Insert(data, rootPtr->m_rightPtr);
+	}
+	return rootPtr;
+}
+
+template<typename T>
+bool BinarySearchTree<T>::Search(T data, BinaryTreeNode<T>* rootPtr) const
+{
+	if (rootPtr == nullptr) return false;
+	else if (rootPtr->m_data == data) return true;
+	else if (data <= rootPtr->m_data) return Search(data, rootPtr->m_leftPtr);
+	else return Search(data, rootPtr->m_rightPtr);
 }
 
 template<typename T>
@@ -90,16 +128,8 @@ void BinarySearchTree<T>::InsertRec(T data)
 }
 
 template<typename T>
-BinaryTreeNode<T>* BinarySearchTree<T>::Insert(T data, BinaryTreeNode<T>* rootPtr)
+bool BinarySearchTree<T>::SearchRec(T data) const
 {
-	if (rootPtr == nullptr) {
-		rootPtr = GetANewNode(data);
-	}
-	else if (data <= rootPtr->m_data) {
-		rootPtr->m_leftPtr = Insert(data, rootPtr->m_leftPtr);
-	}
-	else{
-		rootPtr->m_rightPtr = Insert(data, rootPtr->m_rightPtr);
-	}
-	return rootPtr;
+	return Search(data, m_rootPrt);
 }
+
